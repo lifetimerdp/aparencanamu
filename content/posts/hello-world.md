d1e60113691d36b55eeb5d739a78ef73a95786a8
doni setiawan <donisetiawanprojects@gmail.com>
2024-06-03 17:56:12 +0800

    Initial commit

---
+++ content/posts/hello-world.md
@@ -0,0 +1,5 @@
++++
+title = 'Hello World'
+date = 2024-06-02T08:35:46Z
+draft = true
++++
---
+++ static/js/main.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Apa Rencanamu website loaded');
+});
